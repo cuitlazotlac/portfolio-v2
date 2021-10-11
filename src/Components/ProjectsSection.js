8989904f0f3fc4300ef31736b72e9f4e5760b42d
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-10-11 18:58:25 -0400

    adding the projects carrousel on the home page

---
+++ src/Components/ProjectsSection.js
@@ -0,0 +1,107 @@
+import React from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, { Navigation } from "swiper";
+import ProjectItem from "./ProjectItem";
+import SectionTitle from "./SectionTitle";
+import Title from "./Title";
+import "swiper/swiper-bundle.min.css";
+import showcases from "../data/showcase";
+import { useTranslation } from "react-i18next";
+
+SwiperCore.use([Navigation]);
+
+export default function ProjectsSection() {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <ProjectSectionStyle>
+      <div className="container">
+        <Title title={t("HomePage.CarousselTitle")} span={t("HomePage.CarousselSubtitle")} />
+        <div className="projects__allItems">
+          <Swiper
+            spaceBetween={30}
+            slidesPerView={1}
+            navigation
+            breakpoints={{
+              // when window width is >= 640px
+              640: {
+                slidesPerView: 1,
+              },
+              // when window width is >= 768px
+              768: {
+                slidesPerView: 2,
+              },
+              // when window width is >= 1200px
+              1200: {
+                slidesPerView: 2,
+              },
+            }}
+          >
+            {showcases.map((showcase, index) => {
+              if (index >= 5) return;
+              return (
+                <SwiperSlide key={showcase.id}>
+                  <ProjectItem
+                    title={showcase.name}
+                    img={showcase.img}
+                    desc={showcase.desc}
+                  />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
+      </div>
+    </ProjectSectionStyle>
+  );
+}
+
+const ProjectSectionStyle = styled.div`
+  font-family: "Noto Sans JP", sans-serif;
+  // padding: 10rem 0;
+  padding-top: 4rem;
+  .projects__allItems {
+    display: flex;
+    gap: 3rem;
+    margin-top: 5rem;
+  }
+  .swiper-container {
+    padding-top: 5rem;
+    max-width: 100%;
+  }
+  .swiper-button-prev,
+  .swiper-button-next {
+    position: absolute;
+    height: 50px;
+    width: 50px;
+    background: var(--deep-dark);
+    z-index: 10;
+    right: 60px;
+    left: auto;
+    top: 0;
+    transform: translateY(50%);
+    color: var(--gray-1);
+    border-radius: 8px;
+  }
+  .swiper-button-next {
+    right: 0;
+  }
+  .swiper-button-prev::after,
+  .swiper-button-next::after {
+    font-size: 2rem;
+  }
+  @media only screen and (max-width: 768px) {
+    .projects__allItems {
+      flex-direction: column;
+      max-width: 400px;
+      margin: 0 auto;
+      margin-top: 7rem;
+      gap: 5rem;
+      .projectItem__img {
+        width: 100%;
+        background-color: #383838;
+      }
+    }
+  }
+`;
---
+++ src/data/showcase.js
@@ -0,0 +1,46 @@
+import { v4 as uuidv4 } from 'uuid';
+import ProjectImg from '../resources/img/credly.png';
+import UTrackerImg from '../resources/img/popcorn.png';
+import GreenCtgImg from '../resources/img/picture.png';
+import CoinTrackerImg from '../resources/img/picture_old.png';
+import CavinImg from '../resources/img/picture__OLD.png';
+
+const showcases = [
+  {
+    id: uuidv4(),
+    name: 'U Tracker',
+    desc:
+      'An application to track your all data from one place. I developed the website and the mobile app',
+    img: UTrackerImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'Green CTG',
+    desc:
+      'An app to help people to get an overview of how they can make the city beautiful.',
+    img: GreenCtgImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'Coin Tracker',
+    desc:
+      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
+    img: CoinTrackerImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'Cavins Portfolio',
+    desc:
+      'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
+    img: CavinImg,
+  },
+  {
+    id: uuidv4(),
+    name: 'Tracking Soft',
+    desc:
+      'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
+    img: ProjectImg,
+  },
+];
+
+export default showcases;
